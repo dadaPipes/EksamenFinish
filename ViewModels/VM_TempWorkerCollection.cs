764d94e53537b7d540235fbe208aa88848c573fa
Oliver <markoliverstockflethstaf@gmail.com>
2023-01-30 22:36:15 +0100

    Last

--- ViewModels/VM_TempWorkerCollection.cs
+++ ViewModels/VM_TempWorkerCollection.cs
@@ -1,27 +1,50 @@
-﻿using EksamenFinish.Models;
-using EksamenFinish.Services;
+﻿using EksamenFinish.Services;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace EksamenFinish.ViewModels
 {
     //ViewModel for TempWorker collection
-    public class VM_TempWorkerCollection : IViewModel
+    public class VM_TempWorkerCollection : IViewModel, INotifyPropertyChanged
     {
         public VM_TempWorker TempWorker { get; set; }
-        public ObservableCollection<VM_TempWorker> TempWorkers { get; set; }
+
         private S_TempWorkerRepository s_tempWorkerRepository { get; set; }
 
         public VM_TempWorkerCollection()
         {
-            TempWorkers = new ObservableCollection<VM_TempWorker>();
             s_tempWorkerRepository = new S_TempWorkerRepository();
         }
 
-        public ObservableCollection<VM_TempWorker> GetTempWorkers(VM_TempWorker vm_tempWorker)
+        private ObservableCollection<VM_TempWorker> _tempWorkers;
+
+        public ObservableCollection<VM_TempWorker> TempWorkers
+        {
+            get { return _tempWorkers; }
+            set
+            {
+                _tempWorkers = value;
+                OnPropertyChanged(nameof(TempWorkers));
+            }
+        }
+
+        public void GetTempWorkers(VM_TempWorker vm_tempWorker)
         {
             List<VM_TempWorker> tempWorkers = s_tempWorkerRepository.SearchTempWorkers(vm_tempWorker);
-            return new ObservableCollection<VM_TempWorker>(tempWorkers);
+            TempWorkers = new ObservableCollection<VM_TempWorker>(tempWorkers);
+            OnPropertyChanged(nameof(TempWorkers));
         }
+
+        #region INotifyPropertyChanged Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion INotifyPropertyChanged Implementation
     }
 }
